f43f9618cfd76d5cc7fb7b60155e6a04bd117ad1
Yurii <yuriy dot chalov03 at gmail dot com>
2022-09-29 11:11:44 +0200

    Add tests

--- src/test.cpp
+++ src/test.cpp
@@ -6,7 +6,7 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 
-TEST(MainTest, CorrectCountOfWordsTest)
+TEST(MainTest, CorrectCountOfWordsTest1)
 {
     char **list;
     FILE *f = fopen("main.txt", "r");
@@ -26,6 +26,53 @@ TEST(MainTest, CorrectCountOfWordsTest)
     ASSERT_EQ(25, StringListSize(list));
 }
 
+TEST(MainTest, CorrectCountOfWordsTest2)
+{
+    char **list;
+    StringListInit(&list);
+    StringListAdd(list, "Hello");
+    StringListAdd(list, "World");
+    StringListAdd(list, "How");
+    ASSERT_EQ(3, StringListSize(list));
+}
+
+TEST(MainTest, CorrectCountOfWordsTest3)
+{
+    char **list;
+    StringListInit(&list);
+    StringListAdd(list, "Hello");
+    StringListAdd(list, "World");
+    StringListAdd(list, "How");
+    StringListRemove(list, "How");
+    ASSERT_EQ(2, StringListSize(list));
+}
+
+TEST(MainTest, CorrectCountOfWordsTest4)
+{
+    char **list;
+    StringListInit(&list);
+    StringListAdd(list, "Hello");
+    StringListAdd(list, "World");
+    StringListAdd(list, "How");
+    StringListAdd(list, "How");
+    StringListAdd(list, "World");
+    StringListRemoveDuplicates(list);
+    ASSERT_EQ(3, StringListSize(list));
+}
+
+TEST(MainTest, CorrectIndex1)
+{
+    char **list;
+    StringListInit(&list);
+    StringListAdd(list, "Hello");
+    StringListAdd(list, "World");
+    StringListAdd(list, "How");
+    StringListAdd(list, "How");
+    StringListAdd(list, "World");
+    StringListRemoveDuplicates(list);
+    ASSERT_EQ(0, StringListIndexOf(list, "Hello"));
+}
+ 
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
